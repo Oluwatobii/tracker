9596eb220e809397c3650566b62b5424dbfaeb40
Oluwatobii <bellotobi10@gmail.com>
2022-06-25 03:59:36 -0400

    remove comment

--- components/HomePage/index.js
+++ components/HomePage/index.js
@@ -38,27 +38,3 @@ export default function HomePage() {
     </Stack>
   )
 }
-
-/*
-export default function HomePage() {
-  const client = useClient()
-  const [value, setValue] = useState(false)
-  useEffect(() => {
-    const getPorojects = async () => {
-      try {
-        const response = await client('/api/project')
-      } catch (error) {
-        console.error('Home Page request projets error: ', error)
-      }
-    }
-    getPorojects()
-  }, [value])
-
-  return (
-    <Button colorScheme="blue" onClick={() => setValue(value => !value)}>
-      {' '}
-      Get Projects
-    </Button>
-  )
-}
-*/
